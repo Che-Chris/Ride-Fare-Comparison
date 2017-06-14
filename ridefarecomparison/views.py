06c73bf97f1683f0b24d6e5ad53110d4c6915bc0
Christopher Che <cmche0221@gmail.com>
2017-06-14 14:48:36 -0400

    Using geocoder

--- ridefarecomparison/views.py
+++ ridefarecomparison/views.py
@@ -1,9 +1,10 @@
 import requests
+import geocoder
 
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import config
-from functions import *
+from . import functions
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,8 @@ def index(request):
 def results(request):
     pickup = request.POST.get("PickupLocation")
     dropoff = request.POST.get("DropoffLocation")
-    return render(request, "results.html", {'pickup': pickup, 'dropoff': dropoff})
+
+    geo_pickup = geocoder.google(pickup).latlng
+    geo_dropoff = geocoder.google(dropoff).latlng
+
+    return render(request, "results.html", {'pickup': pickup, 'dropoff': dropoff, 'geo_pickup': geo_pickup, 'geo_dropoff': geo_dropoff})
