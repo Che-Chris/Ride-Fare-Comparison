4d391fb2e0fddcda21f2671054f10b194dfb9e59
guitar4444 <zachary.koch@stonybrook.edu>
2017-06-14 15:28:59 -0400

    Added seat count to getUberPrice param

--- ridefarecomparison/functions.py
+++ ridefarecomparison/functions.py
@@ -1,5 +1,5 @@
 import requests
 
-def getUberPrice(start_latitude, start_longitude, end_latitude, end_longitude):
-    params = {'start_latitude':start_latitude, 'start_longitude': start_longitude, 'end_latitude':end_latitude, 'end_longitude': end_longitude}
+def getUberPrice(start_latitude, start_longitude, end_latitude, end_longitude, SeatCount):
+    params = {'start_latitude':start_latitude, 'start_longitude': start_longitude, 'end_latitude':end_latitude, 'end_longitude': end_longitude, 'SeatCount': SeatCount}
     uberPrice = requests.get('https://api.uber.com/v1.2/estimates/price', params=params)
