3016201f1e8aaf72a7387aedfa2941f5bb8f8ec7
Christopher Che <cmche0221@gmail.com>
2017-06-21 15:47:11 -0400

    Added results.js

---
+++ ridefarecomparison/static/scripts/results.js
@@ -0,0 +1,4 @@
+function default() {
+  $('input[name="PickupLocation"]').val("8 Ogden Avenue, East Williston, NY, United States")
+  $('input[name="PickupLocation"]').val("19 Shortridge Drive, Mineola, NY, United States")
+}
