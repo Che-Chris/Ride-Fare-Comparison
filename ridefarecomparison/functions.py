3e61ec55739cfe772e783f56691ee93121ff1190
Christopher Che <cmche0221@gmail.com>
2017-06-21 14:47:27 -0400

    Setting up lyft prices

--- ridefarecomparison/functions.py
+++ ridefarecomparison/functions.py
@@ -2,14 +2,17 @@ import collections
 
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
+from lyft_rides.auth import ClientCredentialGrant
+from lyft_rides.session import Session
+from lyft_rides.client import LyftRidesClient
+from lyft_rides.auth import AuthorizationCodeGrant
 from config import *
 
-
-session = Session(server_token=UBER_SERVER_TOKEN)
-client = UberRidesClient(session)
-
 def getUberPrice(start_latitude, start_longitude, end_latitude, end_longitude, seat_count):
-    response = client.get_price_estimates(start_latitude=start_latitude, start_longitude=start_longitude, end_latitude=end_latitude, end_longitude=end_longitude, seat_count=seat_count)
+    uber_session = Session(server_token=UBER_SERVER_TOKEN)
+    uber_client = UberRidesClient(uber_session)
+
+    response = uber_client.get_price_estimates(start_latitude=start_latitude, start_longitude=start_longitude, end_latitude=end_latitude, end_longitude=end_longitude, seat_count=seat_count)
 
     estimate = response.json.get('prices')
 
@@ -22,3 +25,17 @@ def getUberPrice(start_latitude, start_longitude, end_latitude, end_longitude, s
         results.append(tempdict)
 
     return results
+
+def getLyftPrices():
+    lyft_auth_flow = ClientCredentialGrant(client_id=YOUR_CLIENT_ID, client_secret=YOUR_CLIENT_SECRET, scopes=YOUR_PERMISSION_SCOPES)
+    lyft_session = lyft_auth_flow.get_session()
+
+    lyft_client = LyftRidesClient(session)
+    response = client.get_cost(37.7833, -122.4167)
+    cost = response.json.get('cost_estimates')
+
+    # lyft_auth_flow = AuthorizationCodeGrant(
+    #     LYFT_CLIENT_ID,
+    #     LYFT_CLIENT_SECRET,
+    # )
+    # lyft_auth_url = auth_flow.get_authorization_url()
